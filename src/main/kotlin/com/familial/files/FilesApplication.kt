e94978c205513025ed040185fc3f01e23aab88a7
Jérôme Guidon <guidon@live.fr>
2021-04-03 22:58:05 +0200

    Initial project commit

---
+++ src/main/kotlin/com/familial/files/FilesApplication.kt
@@ -0,0 +1,11 @@
+package com.familial.files
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilesApplication
+
+fun main(args: Array<String>) {
+	runApplication<FilesApplication>(*args)
+}
---
+++ src/test/kotlin/com/familial/files/FilesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.familial.files
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FilesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
